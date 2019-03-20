7d45ba01cb4c930acbf14c4e8e9268b14ce0e8c3
Jakob Wulf-Eck <jakobweck@gmail.com>
2019-03-20 12:54:02 -0500

    building out main

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,30 @@
 #include "mpi.h"
 
 int main(int argc, char *argv[]) {
-	std::cout << "Hello Easy C++ project!" << std::endl;
+	MPI_Init(&argc, &argv);
+	int rank, communicatorSize;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &communicatorSize);
+
+	if(rank == 0) rank0(communicatorSize);
+	else if (rank==1) errorHandlerRank(1);
+	else ranki(rank);
+
+	MPI_Finalize();
+	return 0;
+}
+
+
+void rank0(int communicatorSize)
+{
+
+}
+
+void ranki(int rank)
+{
+
+}
+void errorHandlerRank(int rank)
+{
+
 }
